50747f223443d5fb81dd3b3943d6f383b47625f4
Himalee <himalee.tailor@gmail.com>
2018-04-16 14:12:45 +0100

    Used a regular expression

--- TipCalculator.rb
+++ TipCalculator.rb
@@ -7,37 +7,50 @@ end
 
 def first_question
   print "What is the bill amount? " #can we use a while loop? Need to make sure the last amount is used in the next function?
-  @bill_amount = gets.chomp.to_f #$ turns variable into a global variable
-    if @bill_amount > 0 #look into regular expressions. @ is an instance variable vs a local variable
-      second_question
-    else
-      puts "Please enter a positive amount"
+  @bill_amount = gets.chomp #$ turns variable into a global variable
+    # if @bill_amount > 0 #look into regular expressions. @ is an instance variable vs a local variable
+    #   second_question
+    if
+      check(@bill_amount)
+      puts "Please enter a number"
       first_question
+    else
+      #puts "Please enter a positive amount"
+      second_question
     end
 end
 
-private
 
-# def check_number
+private
 
 
 def second_question
   print "What is the tip percentage? "
-  @tip_rate = gets.chomp.to_f
-  if @tip_rate > 0
-    answer
-  else
-    puts "Please enter a positive rate"
+  @tip_rate = gets.chomp
+  #if @tip_rate > 0
+    #answer
+  if
+    check(@tip_rate)
+    puts "Please enter a number"
     second_question
+  else
+    #puts "Please enter a positive rate"
+    answer
   end
 end
 
+def check(input)
+  /[a-z]/ =~ input # =~ is known as the "match operator"
+                # can be used to match a string against a regular expression.
+end             # /\D/ - A non-digit character ([^0-9])
+                # means if the string contains the letters a-z
+
 def sub_total
-  @bill_amount
+  (@bill_amount.to_f).round(2)
 end
 
 def tip_amount
-  (@bill_amount * (@tip_rate / 100)).round(2)
+  (@bill_amount.to_f * (@tip_rate.to_f / 100)).round(2)
 end
 
 def grand_total
@@ -66,7 +79,7 @@ end
 end
 
 tcp = TipCalculator.new('£') #new object of the tipcalculator class
-tcd = TipCalculator.new('$') #the currency is the state of the class
+# tcd = TipCalculator.new('$') #the currency is the state of the class
 tcp.first_question #call the funtion
 
 
