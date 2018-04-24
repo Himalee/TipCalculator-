d9eda53e74bd91a2967dd144f4894aa079933cfb
Himalee <himalee.tailor@gmail.com>
2018-04-24 12:31:01 +0100

    Use of a while loop

--- TipCalculator.rb
+++ TipCalculator.rb
@@ -6,44 +6,36 @@ end
 
 
 def first_question
-  print "What is the bill amount? " #can we use a while loop? Need to make sure the last amount is used in the next function?
-  @bill_amount = gets.chomp #$ turns variable into a global variable
-    # if @bill_amount > 0 #look into regular expressions. @ is an instance variable vs a local variable
-    #   second_question
-    if
-      check(@bill_amount)
+  print "What is the bill amount? " # Can we use a while loop? Need to make sure the last amount is used in the next function?
+  @bill_amount = gets.chomp #$ turns variable into a global variable, @ is an instance variable vs a local variable
+    if is_a_letter?(@bill_amount)
       puts "Please enter a number"
       first_question
     else
-      #puts "Please enter a positive amount"
       second_question
     end
 end
 
-
 private
-
 
 def second_question
   print "What is the tip percentage? "
   @tip_rate = gets.chomp
-  #if @tip_rate > 0
-    #answer
-  if
-    check(@tip_rate)
-    puts "Please enter a number"
-    second_question
+    if is_a_letter?(@tip_rate)
+      puts "Please enter a number"
+      second_question
   else
-    #puts "Please enter a positive rate"
-    answer
+    output
   end
 end
 
-def check(input)
-  /[a-z]/ =~ input # =~ is known as the "match operator"
-                # can be used to match a string against a regular expression.
-end             # /\D/ - A non-digit character ([^0-9])
-                # means if the string contains the letters a-z
+
+
+# =~ is known as the "match operator". Used to match a string against a regular expression.
+
+def is_a_letter?(user_input)
+  /\D/ =~ user_input # /\D/ - A non-digit character ([^0-9])
+end
 
 def sub_total
   (@bill_amount.to_f).round(2)
@@ -57,33 +49,14 @@ def grand_total
   sub_total + tip_amount
 end
 
-# @currency = "£"
-# $bill = "#{@currency}"
-  #$tip_amount = $currency(bill * ($tip_rate / 100)).round(2)
-
-
-# def
-# $bill = $bill_amount
-# tip_amount = $bill_amount * ($tip_rate / 100)).round(2)
-# total_pay = $bill_amount + ($bill_amount * ($tip_rate / 100))).round(2)
-
-#$currency = "£"
-
-def answer
+def output
   puts "This bill amount is #{@currency}#{sub_total}"
-  # puts "The bill amount is #{$currency}#{($bill_amount).round(2)}"
   puts "The tip amount is #{@currency}#{tip_amount}"
-  puts "The total to pay is #{@currency}#{grand_total}" #take calculations out and put in a separate funtion.
+  puts "The total to pay is #{@currency}#{grand_total}"
 end
 
 end
 
-tcp = TipCalculator.new('£') #new object of the tipcalculator class
-# tcd = TipCalculator.new('$') #the currency is the state of the class
-tcp.first_question #call the funtion
-
-# puts "The bill amount is #{bill_amount}"
-# puts "The tip amount is #{(bill_amount * (tip_rate1 / 100)).round(2)}"
-# puts "The total to pay is #{(bill_amount + (bill_amount * (tip_rate1 / 100))).round(2)}."
-
-#first_question
+# A new object of the tipcalculator class:
+tpc = TipCalculator.new('£') # The currency is the state of the class
+tpc.first_question # Calls the function
--- while_loop.rb
+++ while_loop.rb
@@ -1,17 +1,69 @@
-prompt = "> "
-puts "Question asking for 1 or 2."
-print prompt
+def first_question
+puts "What is the bill amount?"
 
-while user_input = gets.chomp # loop while getting user input
-  case user_input
-  when "1"
-    puts "First response"
-    break # make sure to break so you don't ask again
-  when "2"
-    puts "Second response"
-    break # and again
+while @bill_amount = gets.chomp
+  case @bill_amount
+  when /\D/
+    puts "Please enter a positive amount"
   else
-    puts "Please select either 1 or 2"
-    print prompt # print the prompt, so the user knows to re-enter input
+    second_question
   end
 end
+end
+
+def second_question
+puts "What is the tip percentage?"
+
+while @tip_amount = gets.chomp
+  case @tip_amount
+  when /\D/
+    puts "Please enter a positive amount"
+  else
+    output
+  end
+end
+end
+
+def sub_total
+  (@bill_amount.to_f).round(2)
+end
+
+def tip_amount
+  (@bill_amount.to_f * (@tip_rate.to_f / 100)).round(2)
+end
+
+def grand_total
+  sub_total + tip_amount
+end
+
+def currency
+  "£"
+end
+
+def output
+  puts "This bill amount is #{currency}#{sub_total}"
+  puts "The tip amount is #{currency}#{tip_amount}"
+  puts "The total to pay is #{currency}#{grand_total}"
+  first_question
+end
+
+first_question
+
+
+# prompt = "> "
+# puts "Question asking for 1 or 2."
+# print prompt
+#
+# while user_input = gets.chomp # loop while getting user input
+#   case user_input
+#   when "1"
+#     puts "First response"
+#     break # make sure to break so you don't ask again
+#   when "2"
+#     puts "Second response"
+#     break # and again
+#   else
+#     puts "Please select either 1 or 2"
+#     print prompt # print the prompt, so the user knows to re-enter input
+#   end
+# end
